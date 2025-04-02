e96da0378782442e0f94c368a27595fdd0d6dd60
DaRkNeSs-ByTnode <edison_travain@outlook.com>
2025-04-01 22:05:38 -0300

    init

---
+++ ALURA/scripit.js
@@ -0,0 +1,9 @@
+function conversor() {
+  let valoremreais = prompt("Digite o valor em reais: ");
+  let valoreuros = valoremreais * 5.76;
+  let valorDolar = valoremreais * 5.25;
+  let valorLibra = valoremreais * 6.7;
+  let valorYen = valoremreais * 0.037;
+  let valorPeso = valoremreais * 0.032;
+}
+
