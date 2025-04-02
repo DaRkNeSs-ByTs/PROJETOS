ed926b4beeb246c2e7f2370a0d2f99e885efba75
DaRkNeSs-ByTnode <edison_travain@outlook.com>
2025-04-01 22:27:27 -0300

    init

--- ALURA/scripit.js
+++ ALURA/scripit.js
@@ -1,21 +1,28 @@
 function conversor() {
   let valoremreais = prompt("Digite o valor em reais: ");
-  let valoreuros = valoremreais * 5.76;
-  let valorDolar = valoremreais * 5.25;
-  let valorLibra = valoremreais * 6.7;
-  let valorYen = valoremreais * 0.037;
-  let valorPeso = valoremreais * 0.032;
+
+  valoremreais = parseFloat(valoremreais);
+  if (isNaN(valoremreais) || valoremreais < 0) {
+    alert("Por favor, digite um valor numérico válido!");
+    return;
+  }
+
+  let valoreuros = (valoremreais * 5.76).toFixed(2);
+  let valorDolar = (valoremreais * 5.25).toFixed(2);
+  let valorLibra = (valoremreais * 6.7).toFixed(2);
+  let valorYen = (valoremreais * 0.037).toFixed(2);
+  let valorPeso = (valoremreais * 0.032).toFixed(2);
 
   alert(
-    "O valor em euros é: " +
+    "O valor em euros é: €" +
       valoreuros +
-      "\nO valor em dólares é: " +
+      "\nO valor em dólares é: $" +
       valorDolar +
-      "\nO valor em libras é: " +
+      "\nO valor em libras é: £" +
       valorLibra +
-      "\nO valor em yens é: " +
+      "\nO valor em yens é: ¥" +
       valorYen +
-      "\nO valor em pesos é: " +
+      "\nO valor em pesos é: $" +
       valorPeso
   );
 }
