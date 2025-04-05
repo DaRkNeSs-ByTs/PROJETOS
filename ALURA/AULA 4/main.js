e3484ef82cf9cbf101293f839e167431ce8ffad0
DaRkNeSs-ByTnode <edison_travain@outlook.com>
2025-04-05 13:52:43 -0300

    commit

---
+++ ALURA/AULA 4/main.js
@@ -0,0 +1,17 @@
+personagem = ["", "", "", ""];
+viloes = ["", "", "", ""];
+
+forcapersonagem = 0;
+forcaviloes = 0;
+
+for (let i = 0; i < 4; i++) {
+  escolhapersonagem = prompt("Digite o nome do personagem " + (i + 1));
+  personagem[i] - escolhapersonagem;
+
+
+
+
+
+
+  
+}
