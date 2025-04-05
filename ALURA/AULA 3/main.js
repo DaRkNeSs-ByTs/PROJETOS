dd27d7c10cc352fd16540e4a44dcf8b7eb87a27f
DaRkNeSs-ByTnode <edison_travain@outlook.com>
2025-04-05 12:35:05 -0300

    commit

--- ALURA/AULA 3/main.js
+++ ALURA/AULA 3/main.js
@@ -1,30 +1,35 @@
 function jogar() {
-  var numero = Math.floor(Math.random() * 10) + 1;
-  var tentativas = 0;
-  var maxTentativas = 5;
-
-  while (tentativas < maxTentativas) {
-    var palpite = parseInt(prompt("Adivinhe o número entre 1 e 10:"));
-
-    if (palpite === numero) {
-      alert(
-        "Parabéns! Você adivinhou o número em " +
-          (tentativas + 1) +
-          " tentativas."
-      );
+  prompt("digite um número entre 1 e 5");
+  var numero = Math.floor(Math.random() * 5) + 1;
+  var tentativas = 3;
+  var ganhou = false;
+  var numeroUsuario = parseInt(prompt("Digite um número entre 1 e 5: "));
+  while (tentativas > 0) {
+    if (numeroUsuario === numero) {
+      ganhou = true;
       break;
-    } else if (palpite < numero) {
-      alert("Muito baixo! Tente novamente.");
     } else {
-      alert("Muito alto! Tente novamente.");
+      tentativas--;
+      if (tentativas > 0) {
+        numeroUsuario = parseInt(
+          prompt(
+            "Número errado! Tente novamente. Você ainda tem " +
+              tentativas +
+              " tentativas."
+          )
+        );
+      }
     }
-
-    tentativas++;
-
-    if (tentativas === maxTentativas) {
-      alert(
-        "Você não conseguiu adivinhar o número. O número era " + numero + "."
-      );
-    }
+  }
+  if (ganhou) {
+    alert("Parabéns! Você acertou o número " + numero + "!");
+  } else {
+    alert("Você perdeu! O número era " + numero + ".");
+  }
+  var jogarNovamente = confirm("Deseja jogar novamente?");
+  if (jogarNovamente) {
+    jogar();
+  } else {
+    alert("Obrigado por jogar!");
   }
 }
