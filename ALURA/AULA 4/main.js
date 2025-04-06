d9e08efc7f8dfa716e2eeeef191f89fc5a27ec18
DaRkNeSs-ByTnode <edison_travain@outlook.com>
2025-04-06 11:35:31 -0300

    commit

--- ALURA/AULA 4/main.js
+++ ALURA/AULA 4/main.js
@@ -1,17 +1,38 @@
-personagem = ["", "", "", ""];
-viloes = ["", "", "", ""];
+let personagem = ["", "", ""];
+let viloes = ["Loki", "Thanos", "Thor"];
 
-forcapersonagem = 0;
-forcaviloes = 0;
+let forcapersonagem = 0;
+let forcaviloes = 0;
 
-for (let i = 0; i < 4; i++) {
-  escolhapersonagem = prompt("Digite o nome do personagem " + (i + 1));
-  personagem[i] - escolhapersonagem;
+alert("Bem-vindo ao jogo de RPG! Escolha os personagens");
 
+for (let i = 0; i < 3; i++) {
+  let escolhapersonagem = prompt("Digite o nome do personagem " + (i + 1));
+  personagem[i] = escolhapersonagem;
+  forcapersonagem += Math.floor(Math.random() * 10) + 1;
+}
 
+for (let i = 0; i < 3; i++) {
+  let indicealeatorio = Math.floor(Math.random() * 3);
+  viloes[i] = viloes[indicealeatorio];
+  forcaviloes += Math.floor(Math.random() * 10) + 1;
+}
 
-
-
-
-  
+if (forcapersonagem > forcaviloes) {
+  alert(
+    "Parabéns, você ganhou a batalha com os personagens " +
+      personagem.join(", ") +
+      " contra os vilões " +
+      viloes.join(", ") +
+      "!"
+  );
+} else {
+  alert(
+    "Que pena, você perdeu a batalha com os personagens " +
+      personagem.join(", ") +
+      " contra os vilões " +
+      viloes.join(", ") +
+      "!"
+  );
+  alert("A força dos personagens é " + forcapersonagem);
 }
