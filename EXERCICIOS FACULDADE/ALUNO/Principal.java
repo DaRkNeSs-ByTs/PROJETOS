8fc34f0f9cda0ab0a6cf70b4479d931a9e7317d1
DaRkNeSs-ByTs <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>
2025-08-21 19:40:48 -0300

    feat: implementar classe Aluno e método principal para exibir informações do aluno

---
+++ EXERCICIOS FACULDADE/ALUNO/Principal.java
@@ -0,0 +1,14 @@
+public class Principal {
+    public static void main(String[] args) {
+
+        Aluno aluno1 = new Aluno(
+            "Edison Travain",
+            "Rua das Flores, 123",
+            "14 999-9999",
+            "EdisonTravain@email.com",
+            "2025001"
+        );
+
+        aluno1.exibirinformacoes();
+    }
+}
