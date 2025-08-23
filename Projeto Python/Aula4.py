64be4d41608e32a4250429c6a323d0708f166520
DaRkNeSs-ByTs <edison.travain@outlook.com>
2025-08-23 15:44:55 -0300

    feat: adicionar exemplo de operadores lógicos com verificação de idade e condições

---
+++ Projeto Python/Aula4.py
@@ -0,0 +1,16 @@
+# Aula - Operadores Lógicos
+# and, or, not
+# and = e
+# or = ou
+# not = não
+# Exemplo:
+
+idade =17
+condicionamento_fisico = False
+autorizacao_medica = True
+
+if idade >= 18 and (condicionamento_fisico or autorizacao_medica):
+    print("Pode fazer o teste de aptidão física")
+
+else:
+    print("você não pode fazer o teste")
